3f1453d4b063bf392e19087f1708d1ea338612fc
shorya <shoryajain1999@gmail.com>
2018-07-24 13:50:28 +0530

    Done

--- lms-backend/src/main/java/internship/lms/user/UserController.java
+++ lms-backend/src/main/java/internship/lms/user/UserController.java
@@ -1,9 +1,6 @@
 package internship.lms.user;
 
 import java.util.List;
-
-
-
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.web.bind.annotation.CrossOrigin;
 import org.springframework.web.bind.annotation.PostMapping;
